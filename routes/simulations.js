7f7ae0be4f78bf9e5b9afcb207d16e3b238a609b
justusmattern <justusmattern@gmx.de>
2020-07-21 00:08:10 +0200

    added variable passing for simulationsite.ejs and simulations.ejs

--- routes/simulations.js
+++ routes/simulations.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const simArray = [];
+simArray[0] = {"name": "Single Pendulum", "description": "This is a single pendulum", "reference": "/simulations/single_pendulum"};
+simArray[1] = {"name": "Double Pendulum", "description": "This is a double pendulum", "reference": "/simulations/double_pendulum"};
+
 router.get('/', function(req,res,next){
-    res.render('simulations');
+    res.render('simulations', {simulations: simArray});
 });
 
 module.exports = router;
--- routes/single_pendulum.js
+++ routes/single_pendulum.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req,res,next){
-    res.render('single_pendulum');
+    res.render('simulationsite', {jsfile: '/javascripts/single_pendulum.js'});
 });
 
 module.exports = router;
