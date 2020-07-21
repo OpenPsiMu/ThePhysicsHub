a485d43ff4986cca0f91a57ab03ccf6f40bea571
justusmattern <justus_mattern@gmx.de>
2020-07-22 00:37:27 +0200

    added json in parameters/simulationdata for passing variables, restyled simulation page

---
+++ routes/parameters/simulationdata.js
@@ -0,0 +1,18 @@
+const simulations = [
+    {
+        "name": "Single Pendulum",
+        "description": "This is a single pendulum",
+        "urlName": "single_pendulum",
+        "jsfile": "single_pendulum",
+        "explanation": "Well... This is how a single pendulum works"
+    },
+    {
+        "name": "Double Pendulum With Spring",
+        "description": "This is a double pendulum connected by spring",
+        "urlName": "spring_double_pendulum",
+        "jsfile": "spring_double_pendulum",
+        "explanation": "We're having two pendulums with a spring connecting them, which is pretty cool"
+    }
+];
+
+module.exports = simulations;
--- routes/simulations.js
+++ routes/simulations.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const simArray = [];
-simArray[0] = {"name": "Single Pendulum", "description": "This is a single pendulum", "reference": "/simulations/single_pendulum"};
-simArray[1] = {"name": "Double Pendulum With Spring", "description": "This is a double pendulum connected by spring", "reference": "/simulations/spring_double_pendulum"};
+const simArray = require('./parameters/simulationdata');
 
 router.get('/', function(req,res,next){
     res.render('simulations', {simulations: simArray});
