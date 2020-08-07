56c291e30c55f01ceba88ff9f2bf6c00ae098c03
justusmattern <justus_mattern@gmx.de>
2020-08-08 00:34:40 +0200

    added default image, styled about and contribute page

--- README.md
+++ README.md
@@ -8,5 +8,3 @@
 
 3. Run `python addSimulation.py` in terminal
 
-4. add `<element>.parent('simwrapper');` for UI elements of your simulation file
-
--- app.js
+++ app.js
@@ -34,22 +34,5 @@ app.use('/simulations/spring_double_pendulum', pendulumSpringRouter);
 
 // Stylesheets
 app.use(express.static(__dirname + '/public'));
-//app.use("/styles",express.static(__dirname + "/styles"));
-
-// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-  //next(createError(404));
-//});
-
-//  error handler
-//app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  //res.status(err.status || 500);
-  //res.render('error');
-//});
 
 module.exports = app;
