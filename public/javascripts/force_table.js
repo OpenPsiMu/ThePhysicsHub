175ecc34619cef5b541e7de43830a235819cfab9
justusmattern <justus_mattern@gmx.de>
2020-08-14 15:48:49 +0200

    added text input for mass/angles; enabled weight positioning with mouse

--- public/javascripts/force_table.js
+++ public/javascripts/force_table.js
@@ -1,164 +1,194 @@
 QuickSettings.useExtStyleSheet();
 
-let center;
 const tablePeri = 500;
 const massPeri = 28;
 const g = 9.81;
+const dt = 0.01;
+
+let center;
+let fRes;
+var gui;
+let rect;
+let nodes;
+
 let xRes = 0;
 let yRes = 0;
 let x = 300;
 let y = 300;
-const dt = 0.01;
-let fRes;
-var gui; 
-var massGuis = [];
-var numOfWeights = [1, 2, 3, 4, 5, 6];
-var mass1 = 0;
-var mass2 = 0;
-var mass3 = 0;
-var mass4 = 0;
-var mass5 = 0;
-var mass6 = 0;
-var rad1 = 0;
-var rad2 = 0;
-var rad3 = 0;
-var rad4 = 0;
-var rad5 = 0;
-var rad6 = 0;
 var started = false;
 var myNumber = 10;
-let weight1;
-let rect;
+
+var massGuis = [];
+var numOfWeights = [1, 2, 3, 4, 5, 6];
+
+var mass1 = "0";
+var mass2 = "0";
+var mass3 = "0";
+var mass4 = "0";
+var mass5 = "0";
+var mass6 = "0";
+
+var angle1 = "0";
+var angle2 = "0";
+var angle3 = "0";
+var angle4 = "0";
+var angle5 = "0";
+var angle6 = "0";
 
 
 
-function setup(){
+let pressed = [];
+for (i = 0; i < 6; i++) {
+    pressed[i] = false;
+}
 
-      cnv = createCanvas(1200,600);
-      cnv.parent('simwrapper');
-      simwrapper = document.getElementById('simwrapper');
-      rect = simwrapper.getBoundingClientRect();
-      background(20);
-      strokeWeight(1);
-      stroke(150);
-      for(i=1; i<=6; i++){
-        line(i*1100/8, 0, i*1100/8, 600);
-      }
-      for(i=1; i<=8; i++){
-        line(0, i* 500/7.5, 827, i*500/7.5);
-      }
-      
-      gui1 = createGui('myGui');
-      gui1.addGlobals('numOfWeights');
-      gui1.setPosition(rect.left+800, rect.top+10);
-
-      center = createVector(300,300);    
-      startButton = createButton('Start!');
-      startButton.position(985, 530);
-      startButton.mousePressed(startSim);
-      startButton.parent('simwrapper');
-
-  }
-
-  function draw(){
-    
-    for(i=1; i <= numOfWeights; i++){
-      if(typeof massGuis[i] == 'undefined'){
-      massGuis[i] = createGui('Weight'+i.toString());
-      sliderRange(0,50, 1);
-      massGuis[i].addGlobals('mass'+i.toString());
-      sliderRange(0,2,0.1);
-      massGuis[i].addGlobals('rad'+i.toString());
-      if(i <= 3){
-      massGuis[i].setPosition(rect.left + 800, rect.top+ 96 + 130*(i-1));
-      }else{
-        massGuis[i].setPosition(rect.left + 1005, rect.top + 96 + 130*(i-4));
-      }
-      center = createVector(300,300);  
-        
-    
-      }
-
-  }
-
-      fill('black');
-      table = ellipse(center.x,center.y,tablePeri);
-      
-      masses = [];
-      forces = [];
-
-      weight1 = createMass(mass1,rad1, '1');
-      weight2 = createMass(mass2,rad2, '2');
-      weight3 = createMass(mass3,rad3, '3');
-      weight4 = createMass(mass4,rad4, '4');
-      weight5 = createMass(mass5,rad5, '5');
-      weight6 = createMass(mass6,rad6, '6');
-
-      strokeWeight(2);
-      fill(20);
-      centerMass = ellipse(x, y, 40, 40);
-
-      
+let nodeDefined = [];
 
 
-      if(started){
+function setup() {
 
-
-      fRes = createVector(0,0);
-      forces.forEach(force => fRes.add(force));
-      forces.push(fRes);
-    
-    drawForces();
-
-      forces.forEach(function(force, index){
-          xRes += force.x*dt;
-          yRes += force.y*dt;
-      });
-
-      if(mag(x-center.x,y-center.y) < 0.5 *(tablePeri)){
-      x += xRes*dt;
-      y += yRes*dt;
-    }else{
-        textSize(14);
-        fill('white');
-        strokeWeight(0);
-        text("Resulting force:\nx: " + fRes.x +"N" + "\ny: "+ -fRes.y + "N" + "\n\nTry balancing the forces!", center.x + 40, center.y+40);
-     }
+    cnv = createCanvas(1200, 600);
+    cnv.parent('simwrapper');
+    simwrapper = document.getElementById('simwrapper');
+    rect = simwrapper.getBoundingClientRect();
+    background(20);
+    strokeWeight(1);
+    stroke(150);
+    for (i = 1; i <= 6; i++) {
+        line(i * 1100 / 8, 0, i * 1100 / 8, 600);
+    }
+    for (i = 1; i <= 8; i++) {
+        line(0, i * 500 / 7.5, 827, i * 500 / 7.5);
     }
 
-  }
+    gui1 = createGui('myGui');
+    gui1.addGlobals('numOfWeights');
+    gui1.setPosition(rect.left + 800, rect.top + 10);
 
-  
-function createMass(mass, rad, num){
-    if(mass != 0){
-    fill(155);
+    center = createVector(300, 300);
+    startButton = createButton('Start!');
+    startButton.position(985, 550);
+    startButton.mousePressed(startSim);
+    startButton.parent('simwrapper');
 
-    m = ellipse(center.x+0.5*(tablePeri-(20+0.5*mass))*cos(rad*PI), center.y+0.5*(tablePeri-(20+0.5*mass))*-sin(rad*PI),20+0.5* mass, 20+0.5*mass);
-    masses.push(m);
+}
 
-    force = createVector(mass*g*cos(rad*PI), mass*g*-sin(rad*PI));
-    forces.push(force);
+function draw() {
 
-    fill('white');
-    text(num, center.x+0.5*(tablePeri-(20+0.5*mass))*cos(rad*PI)-3, center.y+0.5*(tablePeri-(20+0.5*mass))*-sin(rad*PI)+3);
+    for (i = 1; i <= numOfWeights; i++) {
+        if (typeof massGuis[i] == 'undefined') {
+            massGuis[i] = createGui('Weight' + i.toString());
+            sliderRange(0, 50, 1);
+            massGuis[i].addGlobals('mass' + i.toString());
+            sliderRange(0, 2, 0.1);
+            massGuis[i].addGlobals('angle' + i.toString());
+            if (i <= 3) {
+                massGuis[i].setPosition(rect.left + 800, rect.top + 96 + 150 * (i - 1));
+            } else {
+                massGuis[i].setPosition(rect.left + 1005, rect.top + 96 + 150 * (i - 4));
+            }
+            center = createVector(300, 300);
+
+
+        }
+
+    }
+    nodes = [];
+    appendUnits();
+
+    fill('black');
+    table = ellipse(center.x, center.y, tablePeri);
+
+    masses = [];
+    angles = [];
+    forces = [];
+
+    weight1 = createMass(mass1, angle1, '1');
+    weight2 = createMass(mass2, angle2, '2');
+    weight3 = createMass(mass3, angle3, '3');
+    weight4 = createMass(mass4, angle4, '4');
+    weight5 = createMass(mass5, angle5, '5');
+    weight6 = createMass(mass6, angle6, '6');
+
+
+    strokeWeight(2);
+    fill(20);
+    centerMass = ellipse(x, y, 40, 40);
+
+
+
+
+    if (started) {
+
+
+        fRes = createVector(0, 0);
+        forces.forEach(force => fRes.add(force));
+        forces.push(fRes);
+
+        drawForces();
+
+        forces.forEach(function(force, index) {
+            xRes += force.x * dt;
+            yRes += force.y * dt;
+        });
+
+        if (mag(x - center.x, y - center.y) < 0.5 * (tablePeri)) {
+            x += xRes * dt;
+            y += yRes * dt;
+        } else {
+            textSize(14);
+            fill('white');
+            strokeWeight(0);
+            text("Resulting force:\nx: " + fRes.x + "N" + "\ny: " + -fRes.y + "N" + "\n\nTry balancing the forces!", center.x + 40, center.y + 40);
+        }
+    }
+
+}
+
+
+function createMass(massStr, angleStr, num) {
+
+    mass = parseInt(massStr);
+    rad = radians(parseInt(angleStr));
+
+    if (mass != 0) {
+        masses.push(mass);
+        angles.push(rad);
+        fill(155);
+
+        if (pressed[parseInt(num) - 1]) {
+            strokeWeight(1);
+            stroke('blue');
+            rad = atan2(center.y - mouseY, mouseX - center.x);
+            m = ellipse(center.x + 0.5 * (tablePeri - (20 + 0.5 * mass)) * cos(rad), center.y + 0.5 * (tablePeri - (20 + 0.5 * mass)) * -sin(rad), 20 + 0.5 * mass, 20 + 0.5 * mass);
+            strokeWeight(0);
+            stroke(155);
+        } else {
+            m = ellipse(center.x + 0.5 * (tablePeri - (20 + 0.5 * mass)) * cos(rad), center.y + 0.5 * (tablePeri - (20 + 0.5 * mass)) * -sin(rad), 20 + 0.5 * mass, 20 + 0.5 * mass);
+        }
+        force = createVector(mass * g * cos(rad), mass * g * -sin(rad));
+        forces.push(force);
+
+        fill('white');
+        text(num, center.x + 0.5 * (tablePeri - (20 + 0.5 * mass)) * cos(rad) - 3, center.y + 0.5 * (tablePeri - (20 + 0.5 * mass)) * -sin(rad) + 3);
     }
 }
 
-function biggestForce(){
+function biggestForce() {
 
-    let strongestForce = createVector(0,0);
+    let strongestForce = createVector(0, 0);
 
-    for(i = 0; i < forces.length; i++){
-        if(strongestForce.mag() < forces[i].mag()){
+    for (i = 0; i < forces.length; i++) {
+        if (strongestForce.mag() < forces[i].mag()) {
             strongestForce = forces[i];
-        }  
+        }
     }
     return strongestForce;
 }
 
-function drawForces(){
-    forces.forEach(function(force, index){
-        adjustedForce = createVector(force.x*0.5*tablePeri/(biggestForce().mag()),force.y*0.5*tablePeri/(biggestForce().mag()));
+function drawForces() {
+    forces.forEach(function(force, index) {
+        adjustedForce = createVector(force.x * 0.5 * tablePeri / (biggestForce().mag()), force.y * 0.5 * tablePeri / (biggestForce().mag()));
         drawArrow(center, adjustedForce, 'white');
     })
 
@@ -177,8 +207,68 @@ function drawArrow(base, vec, myColor) {
     translate(vec.mag() - arrowSize, 0);
     triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
     pop();
-  }
+}
 
-function startSim(){
+function startSim() {
     started = true;
+}
+
+
+function appendUnits() {
+
+    nodes = document.getElementsByClassName('qs_label');
+
+    for (i = 0; i < nodes.length; i++) {
+        if (nodes[i].textContent.includes('mass') && typeof nodeDefined[i] == 'undefined') {
+            var addedText = document.createElement('b');
+            addedText.innerHTML = ' in kg';
+            nodes[i].appendChild(addedText);
+            nodeDefined[i] = true;
+        }
+        if (nodes[i].textContent.includes('angle') && typeof nodeDefined[i] == 'undefined') {
+            var addedText = document.createElement('b');
+            addedText.innerHTML = ' in degrees';
+            nodes[i].appendChild(addedText);
+            nodeDefined[i] = true;
+        }
+    }
+}
+
+function mousePressed() {
+    for (i = 0; i < angles.length; i++) {
+        if (pressed[i]) {
+            rad = atan2(center.y - mouseY, mouseX - center.x);
+            switch (i) {
+                case 0:
+                    angle1 = degrees(rad).toString();
+                    break;
+                case 1:
+                    angle2 = degrees(rad).toString();
+                    break;
+                case 2:
+                    angle3 = degrees(rad).toString();
+                    break;
+                case 3:
+                    angle4 = degrees(rad).toString();
+                    break;
+                case 4:
+                    angle5 = degrees(rad).toString();
+                    break;
+                case 5:
+                    angle6 = degrees(rad).toString();
+                    break;
+
+            }
+
+            pressed[i] = false;
+            let inputs = document.getElementsByClassName('qs_text_input');
+            inputs[2 * i + 1].value = degrees(rad).toString();
+        }
+        if (!pressed[i]) {
+            if (dist(mouseX, mouseY, center.x + 0.5 * (tablePeri - (20 + 0.5 * masses[i])) * cos(angles[i]), center.y + 0.5 * (tablePeri - (20 + 0.5 * masses[i])) * -sin(angles[i])) < mag(20 + 0.5 * mass, 20 + 0.5 * mass)) {
+                pressed[i] = true;
+            }
+        }
+
+    }
 }
