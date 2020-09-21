f9b4519f21298ee2b6d21d229a3efa5a1e852dc5
Advait Lonkar <lonkaradvait@gmail.com>
2020-09-21 21:41:27 +0530

    Label filtering with a bug

--- public/javascripts/utilities.js
+++ public/javascripts/utilities.js
@@ -1,22 +1,54 @@
+/* Adding labels to the sidebar */
 const label_items=document.getElementsByClassName("label-item");
 const labels = [];
 for(var i = 0; i < label_items.length; i++){
-    //console.log(label_items[i].innerHTML);
     if(labels.includes(label_items[i].innerHTML))
         continue;
     else
         labels.push(label_items[i].innerHTML);
 }
-console.log(labels);
 for(var i = 0; i < labels.length; i++){
-    console.log(labels[i]);
-    var label = document.createElement("span");
+    var label = document.createElement("button");
     label.innerHTML = labels[i];
     label.className = 
     document.getElementsByClassName("sim-label")[0].appendChild(label);
 }
 
+/* Add show class to all sims */
+const sims=document.getElementsByClassName("sim-item");
+for(let i = 0; i < sims.length; i++){
+    sims[i].classList.add("show");
+}
 
-//for(int i = 0; i < labels.length; i++){
-//
-//}
+/* Filter the sims with respect to the labels */
+const filter_items=document.getElementsByClassName("[object HTMLButtonElement]");
+console.log(filter_items);
+for(let i = 0; i < filter_items.length; i++){
+    //console.log(filter_items[i]);
+    filter_items[i].addEventListener("click", function(){
+        
+        //console.log(filter_items[i].innerHTML);
+        const sim=document.getElementsByClassName("sim-item");
+        console.log(sim[i].classList);
+        this.classList.add("active");
+
+        for(let j = 0; j < sim.length; j++){
+            const label_values=sim[j].getElementsByClassName("label-item");
+            //console.log(label_values[0].innerHTML);
+            
+            for(let k = 0; k < label_values.length; k++){
+                if(filter_items[i].innerHTML === label_values[k].innerHTML){
+                    console.log(filter_items[i].innerHTML, sim[j])
+                    sim[j].classList.remove("hide");
+                    sim[j].classList.add("show");
+                }
+                else{
+                    sim[j].classList.remove("show");
+                    sim[j].classList.add("hide");
+                }
+                
+            }
+        }
+
+    })
+}
