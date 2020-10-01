88e5a36bf2e0c3139f4622ffd30d229df47cdd0e
xPolar <harismehrzad12@gmail.com>
2020-09-30 18:28:37 -0700

    Edit .py files for readability. Changed .format to f strings. Added docstrings and comments to editAppjs and createRouter in addSimulation.py. Change double quote strings having to escape other double quotes to single quote strings to allow for the removal of \. Removed several lines in both files when opening app.js (Instead of having an appjs variable to only be used once just use the value of appjs where it appjs would be used).

--- addSimulation.py
+++ addSimulation.py
@@ -1,44 +1,52 @@
 def createRouter(urlName):
+    """Create a router file.
+
+    Args:
+        urlName (str): URL name.
+    """
     print("Creating router")
-    router = open("./routes/{}.js".format(urlName), "w+")
-    router.write("const express = require(\'express\');\n") 
+    router = open(f"./routes/{urlName}.js", "w+")
+    router.write("const express = require('express\);\n") 
     router.write("const router = express.Router();\n")
-    router.write("const simArray = require(\'./parameters/simulationdata\');\n")
-    router.write("const index = simArray.findIndex(function (i) { return i.urlName == \"" + urlName + "\" });\n")
-    router.write("const fs = require(\'fs\');\n")
-    router.write("const path = require(\'path\');\n")
-    router.write("const navbar = fs.readFileSync(path.resolve(__dirname, \"parameters/navigationbar.ejs\"), \"utf-8\");\n")
+    router.write("const simArray = require('./parameters/simulationdata');\n")
+    router.write('const index = simArray.findIndex(function (i) { return i.urlName == "' + urlName + '" });\n')
+    router.write("const fs = require('fs');\n")
+    router.write("const path = require('path');\n")
+    router.write('const navbar = fs.readFileSync(path.resolve(__dirname, "parameters/navigationbar.ejs"), \"utf-8\");\n')
     router.write("\n")
-    router.write("router.get(\'/\', function(req,res,next){\n")
-    router.write("res.render(\'simulationsite\', { sim: simArray[index], navbar: navbar });\n")
+    router.write("router.get('/', function(req,res,next){\n")
+    router.write("res.render('simulationsite', { sim: simArray[index], navbar: navbar });\n")
     router.write("});\n")
     router.write("\n")
     router.write("module.exports = router;") 
-    
-
-
 
 def editAppjs(routerName, urlName):
-    lines = []
-    appjs = open("./app.js")
-    for l in appjs:
-        lines.append(l)
+    """Rewrite our app.js file.
 
+    Args:
+        routerName (str): Router name.
+        urlName (str): URL name.
+    """
+    # Open our app.js file, go through each line in the file and add it to a list called lines. 
+    lines = [ line for line in open("./app.js") ]
+
+    # Open app.js in write mode to allow us to empty the file and then rewrite it.
     newAppjs = open("./app.js", "w")
-    for l in lines:
-        if "./routes" in l and lines[lines.index(l)+1] == '\n':
-            newAppjs.write("const {} = require(\'./routes/{}\');\n".format(routerName, urlName))
-    
-        if "app.use(\'/simulations" in l and lines[lines.index(l)+1] == '\n':
-            newAppjs.write("app.use(\'/simulations/{}\', {});\n".format(urlName, routerName))
-    
-        newAppjs.write(l)
 
+    # Go through each element or line in our lines list, run two quick if statements and write something if either of them are valid. Once out of the if statements write the line from the lines list into app.js.
+    for line in lines:
+        if "./routes" in line and lines[lines.index(line) + 1] == '\n':
+            newAppjs.write(f"const {routerName} = require('.routes/{urlName}');\n")
+        if "app.use(\'/simulations" in line and lines[lines.index(line)+1] == '\n':
+            newAppjs.write(f"app.use('simulations/{urlName}', {routerName});\n")
+        newAppjs.write(line)
 
-urlInput = input("Enter your urlName")
-routerInput = input("Enter your routerName")
-check = input("\nstart adding simulations? type: run \n press other key to stop:\nurlName:{}\nrouterName:{}\n".format(urlInput, routerInput))
+# Ask the user for some input.
+urlInput = input("urlName: ")
+routerInput = input("routerName: ")
+check = input(f"\nStart adding simulations? Type: run \n Press another key to stop:\nurlName:{urlInput}\nrouterName:{routerInput}\n")
 
+# Call editAppjs and creteRouter if the user inputs "run" in our check input, otherwise call exit.
 if check == "run":
     editAppjs(routerInput, urlInput)
     createRouter(urlInput)
