c0e929bb44a33c5a9cbbde40d5262b7c0838fbab
RDxR10 <risirajdey@gmail.com>
2020-10-23 23:39:45 +0530

    Create fluid_mov.js

---
+++ UsefulSketches/fluid_mov.js
@@ -0,0 +1,47 @@
+const num = 7000;
+const range = 6;
+
+let Xk = [];
+let Yk = [];
+
+/*
+var setup = function() {
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	canvas.onmousedown = mouseDown;
+	canvas.onmouseup = mouseUp;
+	canvas.onmousemove = getMousePosition;
+	loopTimer = setInterval(loop, frameDelay);
+}
+*/
+
+function setup() {
+  createCanvas(650, 300);
+  for ( let i = 0; i < num; i++ ) {
+    Xk[i] = width / 2;
+    Yk[i] = height / 2;
+  }
+  frameRate(30);
+}
+
+function draw() {
+  background(51);
+
+  
+  for ( let i = 1; i < num; i++ ) {
+    Xk[i - 1] = Xk[i];
+    Yk[i - 1] = Yk[i];
+  }
+
+  Xk[num - 1] += random(-range, range);
+  Yk[num - 1] += random(-range, range);
+  Xk[num - 1] = constrain(ax[num - 1], 0, width);
+  Yk[num - 1] = constrain(ay[num - 1], 0, height);
+
+  
+  for ( let j = 1; j < num; j++ ) {
+    let val = j / num * 272.0 + 77;
+    stroke(val);
+    line(Xk[j - 1], Yk[j - 1], Xk[j], Yk[j]);
+  }
+}
